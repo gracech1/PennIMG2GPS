9bef90a8b439308d62d116b3cdfcc02cb3b976e0
gracech1 <182316211+gracech1@users.noreply.github.com>
2026-04-25 19:31:25 -0400

    Initial Commit

---
+++ PennIMG2GPS/PennIMG2GPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennIMG2GPSApp.swift
+//  PennIMG2GPS
+//
+//  Created by Grace Chi on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PennIMG2GPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
